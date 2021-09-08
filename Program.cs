b0f178dd2b936b5f19ae622c345fc35961513ff0
Dara Oladapo <daraoladapo@outlook.com>
2021-09-08 09:11:30 +0100

    added proejct file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnethello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
